d938818052a87b9252d21034119bd0b850058a0f
Zeeshan <zeeshan2006zeeshan@gmail.com>
2025-09-06 18:50:27 +0530

    Basics of java

---
+++ DAY1/BASICS/basics.java
@@ -0,0 +1,92 @@
+//package DAY1.DSA;
+
+//BASICS OF LANGUAGE 
+
+/*
+import java.util.*;
+
+public class basics{
+    public static void main(String args[]){
+     Scanner sc = new Scanner(System.in);
+     
+     int a= sc.nextInt();
+
+     int b= sc.nextInt();
+     
+
+     int sum=a+b;
+     System.out.println(sum);
+    }
+}*/
+
+
+
+/*import java.util.*;
+
+public class basic{
+    public static void main(String args[]){
+    int counter =0;
+    while(counter < 100){
+        System.out.println("Hello World");
+        counter++;
+        
+    }
+    System.out.println("printer hw 100v");
+}
+}
+
+/*import java.util.*;
+
+public class basic{
+    public static void main(String args[]){
+    int counter =0;
+    while(counter < 10){
+        System.out.println(counter);
+        counter++;
+        
+    }
+    System.out.println("printed number 10");
+}
+}
+
+
+
+import java.util.*;
+
+public class basic{
+    public static void main(String args[]){
+      Scanner sc = new Scanner(System.in);
+      int n =sc.nextInt();
+      
+    
+        int counter =1;
+    while(counter <= n){
+        System.out.println(counter);
+        counter++;
+        
+    }
+    System.out.println("printed number " + n);
+}
+}
+
+import java.util.*;
+
+public class basic{
+    public static void main(String args[]){
+      Scanner sc = new Scanner(System.in);
+      int n =sc.nextInt();
+      
+    int sum = 0;
+    int i = 1;
+    while(i <= n){
+        sum +=i;
+        i++;
+    
+        
+        
+    }
+     System.out.println(  sum);  //remeber here there is loop so u should write the print statment outside the loop
+}
+}
+
+ */
