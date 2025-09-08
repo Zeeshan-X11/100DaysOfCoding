c3a717948666207bd59ba8e5b4eafc010d210415
Zeeshan <zeeshan2006zeeshan@gmail.com>
2025-09-08 23:23:19 +0530

    brushing up

---
+++ DAY3/DSA/halfpy.java
@@ -0,0 +1,19 @@
+package DAY3.DSA;
+
+
+
+import java.util.*;
+
+public class halfpy{
+    public static void main(String args[]){
+   int n=4;
+   for (int i =1;i<=n;i++){   //
+    
+    for (int j=1;j<=i;j++){
+          System.out.print(j);   //  System.out.println(i + " * " + j + " = " + (i * j));  
+
+    }
+    System.out.println();  // this will print the next line after printing all the stars in one line
+   }
+}
+}
