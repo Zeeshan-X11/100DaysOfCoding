0566dde5c6cf688838d1fa0f3cd54dc4ea92e4d7
Zeeshan <zeeshan2006zeeshan@gmail.com>
2025-09-08 21:15:31 +0530

    brushing up basic

---
+++ DAY3/DEV/basic.java
@@ -0,0 +1,9 @@
+//package DAY3.DEV;
+
+/*Day 6
+
+Revised HTML + CSS combined for practice.
+
+Learnt about forms styling & pseudo-classes (hover, focus, active).
+
+Built a styled registration form.*/
