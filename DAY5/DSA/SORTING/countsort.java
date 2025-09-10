368b67e5a3ca3e8cceb08afd95053e92ef32a92d
Zeeshan <zeeshan2006zeeshan@gmail.com>
2025-09-10 23:31:46 +0530

    UPDATE

---
+++ DAY5/DSA/SORTING/countsort.java
@@ -0,0 +1,36 @@
+package SORTING;
+public class countsort {
+    public static void countingSort(int arr[]) {
+        // find max
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+
+        int count[] = new int[largest + 1]; // freq array
+
+        // fill freq
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+
+        // sorting
+        int j = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {5, 4, 1, 3, 2};
+        countingSort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
