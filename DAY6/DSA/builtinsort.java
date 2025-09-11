ff523f8070b9d3c8a006c57a4143b06a35125515
Zeeshan <zeeshan2006zeeshan@gmail.com>
2025-09-11 21:21:59 +0530

    UPDATE

---
+++ DAY6/DSA/builtinsort.java
@@ -0,0 +1,14 @@
+package DAY6.DSA;
+
+
+import java.util.Arrays;
+
+public class builtinsort {
+    public static void main(String[] args) {
+        int[] numbers = {5, 2, 9, 1, 6};
+        
+        Arrays.sort(numbers); // Sorts in ascending order
+        
+        System.out.println(Arrays.toString(numbers));
+    }
+}
