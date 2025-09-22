fc2cdc7a1448bffcf3dd43f805148cc2b3ce8e26
Zeeshan <zeeshan2006zeeshan@gmail.com>
2025-09-22 20:49:39 +0530

    UPDATE

---
+++ DAY16/uppertolower.java
@@ -0,0 +1,16 @@
+package STRINGS;
+
+public class uppertolower {
+
+    public static void main(String[] args) {
+        // Sample string in uppercase
+        String str = "HELLO WORLD!";
+
+        // Converting the string to lowercase
+        String lowerStr = str.toLowerCase();
+
+        // Printing the lowercase string
+        System.out.println("Original String: " + str);
+        System.out.println("Lowercase String: " + lowerStr);
+    }
+}
