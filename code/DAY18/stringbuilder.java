223ae489e961d983c7bdbabe6cb92464545c513f
Zeeshan <zeeshan2006zeeshan@gmail.com>
2025-09-25 20:37:26 +0530

    UPDATE

---
+++ code/DAY18/stringbuilder.java
@@ -0,0 +1,17 @@
+package STRINGS;
+
+public class stringbuilder {
+    public static void main (String args[]){
+        StringBuilder sb = new StringBuilder("");
+        for(char ch = 'a';ch <= 'z' ; ch++){
+            sb.append(ch);
+        }//abcde.. //O(n)=26
+        //if not sbuilder then 26 x n.lenght
+  
+    System.out.println(sb);
+}
+
+}
+
+//string builder becz effecency increases 
+
