462fcd47b903892144a44b8e85bce8f89da80696
Zeeshan <zeeshan2006zeeshan@gmail.com>
2025-09-27 23:04:04 +0530

    UPDATE

---
+++ code/DAY19/insertionsort.java
@@ -0,0 +1,27 @@
+package SORTING;
+public class insertionsort {
+    public static void insertionSort(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
+            int curr = arr[i];
+            int prev = i - 1;
+
+            // shifting
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+
+            // place current
+            arr[prev + 1] = curr;
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {5, 4, 1, 3, 2};
+        insertionSort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
