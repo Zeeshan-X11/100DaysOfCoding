842fed62dada5c47dd52285d4bf70797a8fea72f
Zeeshan <zeeshan2006zeeshan@gmail.com>
2025-09-29 21:08:37 +0530

    UPDATE

---
+++ code/DAY20/inbuildsort.java
@@ -0,0 +1,18 @@
+package SORTING;
+import java.util.Arrays;
+
+public class inbuildsort {
+    public static void main(String args[]) {
+        int arr[] = {5, 4, 1, 3, 2};
+
+        // ascending order
+        Arrays.sort(arr);
+
+        // for descending order
+        // Arrays.sort(arr, Collections.reverseOrder()); --> works only for Integer[], not int[]
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
