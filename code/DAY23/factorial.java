4b9a90dd2caf067c1fee16f117bedfa660fc5ff6
Zeeshan <zeeshan2006zeeshan@gmail.com>
2025-10-04 23:14:08 +0530

    UPDATE

---
+++ code/DAY23/factorial.java
@@ -0,0 +1,17 @@
+package RECURSSION.REC1;
+
+public class factorial {
+    public static int fact(int n){
+        if(n==0){
+            return 1;
+        }
+        int fnm1 = fact(n-1);
+        int fn = n * fact(n-1);
+        return fn;
+    }
+    public static void main (String args[]){
+        int n=5;
+       System.out.println(fact(n));
+    }
+}
+//tc = 0(n),space even we dint use variable but it eat memory space/stack space n var 
