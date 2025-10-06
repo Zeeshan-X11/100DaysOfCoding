35eb28283d17fd555908a6040bc12752f30111d9
Zeeshan <zeeshan2006zeeshan@gmail.com>
2025-10-06 20:42:39 +0530

    UPDATE

---
+++ code/DAY24/numbersindecreasing.java
@@ -0,0 +1,16 @@
+package RECURSSION.REC1;
+
+public class numbersindecreasing {
+    public static void printDec(int n){
+        if(n==1){                       //this base case is most imp , if u dont give it give stack over flow error and run infinitly
+            System.out.println(n);  
+            return;
+        }
+        System.out.print(n + " ");
+        printDec(n-1);
+    }
+    public static void main(String args[]){
+        int n=10;
+        printDec(n);//calling 
+    }
+}
