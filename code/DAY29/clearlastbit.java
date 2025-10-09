7563e86ca2111ca5850def5fcfb4813fce1b7491
Zeeshan <zeeshan2006zeeshan@gmail.com>
2025-10-09 23:21:55 +0530

    UPDATE

---
+++ code/DAY29/clearlastbit.java
@@ -0,0 +1,11 @@
+package BITMANULULATION;
+
+public class clearlastbit {
+    public static int clearibits(int n , int i ){
+        int bitmask = (~0)<<i;
+        return n & bitmask;
+    }
+    public static void main(String args[]){
+        System.out.println(clearibits(15,2));
+    }
+}
