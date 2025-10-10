0f28c8cdb2c7ae792e401ff132e441bac4b96012
Zeeshan <zeeshan2006zeeshan@gmail.com>
2025-10-10 21:39:33 +0530

    UPDATE

---
+++ code/DAY31/oddoreven.java
@@ -0,0 +1,18 @@
+package BITMANULULATION;
+
+public class oddoreven {
+    public static void oddoreven(int n){
+   int bitmask =1;
+   if((n&bitmask )== 0) {
+    System.out.println("even number");
+   }else{
+    System.out.println("odd number");
+   }
+   }
+   public static void main(String args[]){
+    oddoreven(3);
+    oddoreven(24);
+   }
+    }
+    
+
