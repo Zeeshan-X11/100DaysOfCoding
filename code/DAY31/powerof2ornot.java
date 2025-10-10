0c7fc75f403f8481ca8f50616446a7d1ed113ef8
Zeeshan <zeeshan2006zeeshan@gmail.com>
2025-10-10 21:39:19 +0530

    UPDATE

---
+++ code/DAY31/powerof2ornot.java
@@ -0,0 +1,12 @@
+package BITMANULULATION;
+
+public class powerof2ornot {
+    public static boolean ispoweroftwo(int n ){
+        return (n&(n-1)) == 0;            //15&16 or 3&4
+    }
+
+    public static void main(String args[]){
+        System.out.println(ispoweroftwo(15));
+    }
+}
+
